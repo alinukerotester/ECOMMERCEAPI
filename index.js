8b3fa46e557c421c42ed451806e1f79bcaeeef1a
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2023-08-14 18:38:11 +0300

    up

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+).then(() => console.log("Conectare DB cu succes!"))
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.listen(5000, () => {
     console.log("Back end server is running !");
